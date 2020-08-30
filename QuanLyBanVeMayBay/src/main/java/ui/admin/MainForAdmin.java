a34e36a95e5c4d98c024cd3e88ee42a7ad048770
= <nguyenanhhao.hcmus@gmail.com>
2020-08-30 22:34:23 +0700

    merge

--- QuanLyBanVeMayBay/src/main/java/ui/admin/MainForAdmin.java
+++ QuanLyBanVeMayBay/src/main/java/ui/admin/MainForAdmin.java
@@ -156,22 +156,6 @@ public class MainForAdmin extends javax.swing.JFrame {
         this.admin = ad;
     }
 
-    public static void main(String[] args) {
-
-        try {
-            //UIManager.setLookAndFeel(new FlatDarkLaf());
-            UIManager.setLookAndFeel(new FlatIntelliJLaf());
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        }
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                MainForAdmin.getInstance().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnLogout;
     private javax.swing.Box.Filler filler1;
